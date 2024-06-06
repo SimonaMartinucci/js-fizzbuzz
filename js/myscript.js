c76158c9cfb9611c6f426840f0127182b2af43fa
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-06-06 17:09:56 +0200

    fix classes in js + css

--- js/myscript.js
+++ js/myscript.js
@@ -2,34 +2,37 @@
 for(let i = 1; i <= 100; i++) {
     console.log(i);
 
+    // richiamo container html
+    const container = document.querySelector(".container");
+
+    // creo nuovo elemento div
+    let box = document.createElement("div");
+
     let messaggio;
 
         // multipli sia di 3 che di 5 stampi “FizzBuzz”
         if(i % 3 === 0 && i % 5 === 0) {
             messaggio = "FizzBuzz"
+            box.classList.add("fizzbuzz")
         // multipli di 3 stampi “Fizz”
         }else if(i % 3 === 0) { 
             messaggio = "Fizz"
+            box.classList.add("fizz")
         // multipli di 5 stampi “Buzz”
         }else if(i % 5 === 0) {
             messaggio = "Buzz"
+            box.classList.add("buzz")
         }else {
             messaggio = i;
+            box.classList.add("box")
         }
 
     console.log(messaggio);
 
+    container.append(box);
 
-// richiamo container html
-const container = document.querySelector(".container");
+    box.append(messaggio);
 
-// creare div da inserire nel container
-const boxDaInserire = `<div class="box">${messaggio}</div>`;
 
-// inserire box all'interno di container
-container.innerHTML += boxDaInserire;
-
-// Richiamare div
-let box = document.querySelector(".box")
 
 }
